1a40c82946ad41fd9158bf235690608331376d9e
Jarrod Steve <gosthunterr02@yahoo.com>
2016-06-22 02:50:59 +0000

    Added the edit category function

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   
+  before_action :set_category, only: [:show, :edit, :update]
   before_action :require_admin, except: [:index, :show]
   
   def index
@@ -11,7 +12,6 @@ class CategoriesController < ApplicationController
   end
   
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
@@ -25,9 +25,24 @@ class CategoriesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @category.update(category_params)
+      flash[:success] = "Category successfully updated"
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
+  end
+  
   private
     def category_params
       params.require(:category).permit(:name)
+    end
+    def set_category
+      @category = Category.find(params[:id])
     end
     
     def require_admin
